a141ffc27b45677e929722043c2604fe33176d50
ovenslove <1905997838@qq.com>
2016-10-10 00:40:57 +0800

    aasq1

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,7 @@ var router = require('koa-router')();
 var mongoose = require("mongoose");
 
 router.get('/', function (ctx, next) {
-    var db=mongoose.connect('mongodb://localhost/mydb');
-    var result=db.find();
-  ctx.body = result;
+  ctx.body = 'nihao ';
 });
 
 module.exports = router;
