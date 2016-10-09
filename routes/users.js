a6f57e78d1ad9765bddb289531df0112f4b6da7b
ovenslove <1905997838@qq.com>
2016-10-10 00:22:24 +0800

    aasq1

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,7 @@
 var router = require('koa-router')();
-var mongoose = require ( 'mongoose' );
-
 
 router.get('/', function (ctx, next) {
-
-    var db=mongoose.connect('mongodb://localhost/my_database');
-    var result=db.book.find();
-    ctx.body=result;
-
+  ctx.body = 'this a users response!';
 });
 
 module.exports = router;
