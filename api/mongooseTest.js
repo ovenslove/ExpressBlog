7217582e3fe0dd31f9206121c440eb1c4a91c29c
ovenslove <1905997838@qq.com>
2016-10-12 23:55:14 +0800

    aaak

--- api/mongooseTest.js
+++ api/mongooseTest.js
@@ -3,15 +3,22 @@ var mongoose=require('mongoose');
 var db =mongoose.createConnection('localhost','blog'); //创建一个数据库连接
 var blogSchema  = new mongoose.Schema({
     title:String,   //定义一个属性name，类型为String
-    author:String   //定义一个属性name，类型为String
+    author:String,   //定义一个属性name，类型为String
+    intr:String,    //简介
+    info:String     //摘要
 });
 
 var blogModel = db.model('blog',blogSchema);
 
-var blogEntity = new blogModel({title:'我的名字就是oven','author':'ssss'});
+// var blogEntity = new blogModel({title:'我就是标题','author':'ovenslove','intr':'我就是简介，看我看我','info':'别动，我是从mongodb出来的摘要'});
 
-console.log(blogEntity.title);
-blogEntity.save();
+for(var i=1;i<20;i++){
+    var blogEntity= new blogModel({title:'我就是标题'+i,'author':'ovenslove','intr':'我就是简介'+i+'，看我看我','info':'别动，我是从mongodb出来的第'+i+'摘要'}).save();
+    console.log(blogEntity.title);
+
+}
+// blogEntity.save();
+/*
 blogModel.find(function (err, blogs) {
     console.log(blogs);
-});
+});*/
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+var mongoose=require('mongoose');
+
+var db =mongoose.createConnection('localhost','blog'); //创建一个数据库连接
+var blogSchema  = new mongoose.Schema({
+    title:String,   //定义一个属性name，类型为String
+    author:String,   //定义一个属性name，类型为String
+    intr:String,    //简介
+    info:String     //摘要
+});
 
 /* GET home page. */
 var data={
   'title':'API首页',
   'list':[
-    {'title':'我是第一个','intr':'这里是我的简介','info':'我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我'},
+    /*{'title':'我是第一个','intr':'这里是我的简介','info':'我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我'},
     {'title':'我是第二个','intr':'这里是我的简介这里是我的简介这里是我的简介','info':'我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我我就是正文啊。看我看我'},
     {'title':'我是第一个','intr':'这里是我的简介','info':'我就是正文啊。看我看我'},
     {'title':'我是第二个我是第二个我是第二个','intr':'这里是我的简介','info':'我就是正文啊。看我看我'},
@@ -18,11 +26,16 @@ var data={
     {'title':'我是第二个我是第二个','intr':'这里是我的简介','info':'我就是正文啊。看我看我'},
     {'title':'我是第二个','intr':'这里是我的简介','info':'我就是正文啊。看我看我'},
     {'title':'我是第二个我是第二个我是第二个我是第二个我是第二个我是第二个','intr':'这里是我的简介','info':'我就是正文啊。看我看我'},
-    {'title':'我是第三个','intr':'这里是我的简介','info':'我就是正文啊。看我看我'}
+    {'title':'我是第三个','intr':'这里是我的简介','info':'我就是正文啊。看我看我'}*/
   ]
 }
+
 router.get('/', function(req, res, next) {
-  res.render('index', data);
+    var blogModel = db.model('blog',blogSchema);
+     blogModel.find(function (err, blogs) {
+        data.list.push(blogs);
+     });
+    res.render('index', data);
 });
 
 module.exports = router;
