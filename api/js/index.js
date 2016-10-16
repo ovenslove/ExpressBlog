44e8111d1d2b755def89a410b4ba47e37eac24b8
ovenslove <1905997838@qq.com>
2016-10-16 13:58:25 +0800

    'aa'

--- api/js/index.js
+++ api/js/index.js
@@ -1,1 +1,22 @@
-console.log('11ssssss');
+/*回到顶部*/
+$(".back2top").on('click',function () {
+    alert("11");
+});
+$(function () {
+    $("body").scrollTop(0);
+});
+
+$("body").on('mousewheel', function(event) {
+    var _top=$("body").scrollTop();
+    if(_top>1000){
+        $(".side-box").slideDown(200);
+    }else {
+        $(".side-box").slideUp(200);
+    }
+    if(_top>2000){
+        $(".back2top").slideDown(200);
+    }else {
+        $(".back2top").slideUp(200);
+    }
+    // console.log();
+});
