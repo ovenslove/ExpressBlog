13964acf966aef8ff66af72915305d4c74214525
ovenslove <1905997838@qq.com>
2016-11-23 14:34:49 +0800

    修改首页

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var router = require('koa-router')();
 
 router.get('/', async function (ctx, next) {
   ctx.state = {
-    title: 'koa2 title'
+    title: '首页'
   };
 
   await ctx.render('index', {
