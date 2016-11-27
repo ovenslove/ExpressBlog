df6c823d695ab58d1533863479cdfd77b0988071
ovenslove <1905997838@qq.com>
2016-11-27 23:10:18 +0800

    :art: add a test API

--- api/routes/testjson.js
+++ api/routes/testjson.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var data={
+  "status":1,
+  "msg":"ok",
+  "data":{
+    "name":"peter",
+    "age":"17"
+  }
+};
+
 /* GET home page. */
 router.get('/testjson', function(req, res, next) {
-  var data={
-    "status":1,
-    "msg":"ok",
-    "data":{
-      "name":"peter",
-      "age":"17"
-    }
-  };
-  res.json(data);
+
+  var _callback = req.query.callback;
+  if (_callback){
+    res.type('text/javascript');
+    res.send(_callback + '(' + JSON.stringify(data) + ')');
+  }
+  else{
+    res.json(data);
+  }
 });
 
 module.exports = router;
