e64b92f54de51b1a11ca8305fb66c239de69b4b4
ovenslove <1905997838@qq.com>
2016-12-01 17:52:23 +0800

    首页重构

--- public/js/main.js
+++
@@ -1,5 +0,0 @@
-function say(word) {
-	// body...
-	console.log('我说了'+word);
-}
-say('你是谁');
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var router = require('koa-router')();
-var mongoose = require("mongoose");
-
-router.get('/', function (ctx, next) {
-  ctx.body = 'nihao ';
-});
-
-module.exports = router;
